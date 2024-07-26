cc9899aed0f9bfbcc55479fe18ab82e05d924c64
faviocabral <faviocabral@gmail.com>
2024-07-26 15:27:49 -0400

    update sweatalert

--- index.php
+++ index.php
@@ -658,6 +658,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <script src="plugins/input-mask/jquery.inputmask.js"></script>
   <script src="plugins/input-mask/jquery.inputmask.date.extensions.js"></script>
   <script src="plugins/input-mask/jquery.inputmask.extensions.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
 	<script type="text/javascript">
 
