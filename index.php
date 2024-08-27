72cf8cbc2ad9e6b6665a2c431770ec2cc778517d
faviocabral <faviocabral@gmail.com>
2024-08-27 18:09:12 -0400

    update in

--- index.php
+++ index.php
@@ -3032,7 +3032,7 @@ async	function NuevoCliente(){
             $.ajax( { method: "POST", url: 'consulta.php', data : {datos : result , funcion: 'insertarOrden' , sucursal : sucu  }, dataType: 'html'})
             .done(function(rs) {
               console.log('paso por control... ');
-              console.log(rs);
+              console.log(rs[0]);
                 //GuardarImagen2(rs); //guardar la imagen del combustible ... 
                 //ImagenOt2(rs); // guardar con el nro de orden la imagen del auto ....
                 alert(rs[0]['callid'])
