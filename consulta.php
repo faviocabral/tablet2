e16f36c61d5c0fc0595c51898705e026ed554595
faviocabral <faviocabral@gmail.com>
2024-10-01 00:53:29 -0400

    update pro

--- consulta.php
+++ consulta.php
@@ -221,7 +221,7 @@ $env = parse_ini_file('.env');
 			  $valor[] = $row;
 		  }
 		  $datos = 	json_encode( $valor );
-		  echo $datos[0]['callid'];
+		  echo $datos[0];
 		  return ;
 
 		  $consulta2 = 
