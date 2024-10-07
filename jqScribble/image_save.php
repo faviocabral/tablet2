18dd91bedd20e4eea0c05234fda35666dd83cb9c
faviocabral <faviocabral@gmail.com>
2024-10-06 22:41:01 -0300

    upd pic

--- jqScribble/image_save.php
+++ jqScribble/image_save.php
@@ -1,5 +1,5 @@
 <?php
-
+echo 'entro para crear la imagen....... ';
 $data = $_POST['imagedata']; 
 //$filename = 'jqScribbleImage.png';
 $asesor = $_POST['asesor']; 
@@ -8,7 +8,8 @@ $filename = md5($asesor) . '.png';
 //Need to remove the stuff at the beginning of the string 
 $data = substr($data, strpos($data, ",")+1); 
 $data = base64_decode($data); 
+echo 'llego 1';
 $imgRes = imagecreatefromstring($data); 
-echo $imgRes;
+echo 'llego 2';
 if($imgRes !== false && imagepng($imgRes, $filename) === true) 
     echo "<img src='jqScribble/{$filename}?" . rand() . "' alt='jqScribble Created Image'/>";
